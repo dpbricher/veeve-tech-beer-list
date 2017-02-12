a75389b315845d945fd9123273a76866337148e2
Dean Richer <dpbricher@gmail.com>
2017-02-12 18:47:27 +0000

    Added ratio and next buyer logic

--- dist/index.js
+++ dist/index.js
@@ -17,18 +17,43 @@
   app.get('/', (req, res)=> {
     let recordList  = db.getRecordList();
     let totalMap    = {};
+    let personList  = [];
 
     recordList.forEach((i)=> {
       for (let v of i) {
-        totalMap[v.name]  = totalMap[v.name] || {};
-        totalMap[v.name].bought   = (totalMap[v.name].bought || 0) +
-          (v.bought || 0);
-        totalMap[v.name].consumed = (totalMap[v.name].consumed || 0) +
-          (v.consumed || 0);
+        let personMap       =
+        totalMap[v.name]    = totalMap[v.name] || {};
+
+        personMap.bought    = (personMap.bought || 0) + (v.bought || 0);
+        personMap.consumed  = (personMap.consumed || 0) + (v.consumed || 0);
       }
     });
 
-    res.render('main', { totalMap:totalMap, recordList:recordList });
+    // convert from map to array
+    for (let name in totalMap) {
+      let personMap = totalMap[name];
+
+      personMap.name  = name;
+      personList.push(personMap);
+    }
+
+    // calculate ratios
+    personList.forEach((personMap)=> {
+      personMap.ratio = personMap.consumed > 0 ?
+        personMap.bought / personMap.consumed : 0;
+    });
+
+    // determine who should be next to buy
+    let lowestRatio = personList.reduce((lowest, personMap)=>
+      Math.min(personMap.ratio, lowest)
+    , Infinity);
+
+    let nextList    = personList.filter((personMap)=>
+      personMap.ratio == lowestRatio
+    );
+
+    res.render('main', { nextBuyersList:nextList, personList:personList,
+      recordList:recordList });
   });
 
   app.get('/create', (req, res)=> res.render('create'));
